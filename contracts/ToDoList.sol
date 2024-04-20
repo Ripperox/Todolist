7bea90dda1490fa0a6886610b95eba60f6e2415b
Ripperox <rishitrdhote@gmail.com>
2024-04-20 10:05:19 +0530

    frontend starting

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 
 contract ToDoList{
     uint256 public taskCount=0;
+
+    struct Task{
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public{
+        createTask("This is my first app");
+    }
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount]= Task(taskCount,_content,false);
+    }
+
 }
